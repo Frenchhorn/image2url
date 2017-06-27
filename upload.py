19c6a8e50fac2c8d725bd5d86b94552f697aace4
CPPQQZ <CPPQQZ@cathaypacific.com>
2017-06-27 14:28:23 +0800

    Add TreadPoolExecutor to upload images

--- upload.py
+++ upload.py
@@ -1,5 +1,6 @@
 import asyncio
 import requests
+import concurrent.futures
 from pprint import pprint
 
 
@@ -8,34 +9,37 @@ WEBSITES = {'url':'https://sm.ms',
             'api':'https://sm.ms/api/upload',
             'params':{'ssl':True, 'format':'json'},
             'files':'smfile'}
+results = {}
+errors = {}
 
 
 def uploadImage(path):
     with open(path, 'rb') as f:
-        req = requests.post('https://sm.ms/api/upload', params={'ssl':True, 'format':'json'}, files={'smfile':f})
+        req = requests.post('https://sm.ms/api/upload', params={'ssl':True, 'format':'json'}, files={'smfile':f}, verify=True)
         res = req.json()
     return res
 
 
-# coroutine
+# ThreadPool
 def uploadImages(paths):
-    async def uploadImage(path):
-        with open(path, 'rb') as f:
-            req = requests.post('https://sm.ms/api/upload', params={'ssl':True, 'format':'json'}, files={'smfile':f})
-            res = req.json()
-            results[path] = res
-    results = {}
-    coroutines = [uploadImage(path) for path in paths]
-    eventLoop = asyncio.get_event_loop()
-    try:
-        eventLoop.run_until_complete(asyncio.wait(coroutines))
-    finally:
-        eventLoop.close()
-    return results
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        future_to_path = {executor.submit(uploadImage, path) : path for path in paths}
+        for future in concurrent.futures.as_completed(future_to_path):
+            path = future_to_path[future]
+            try:
+                results[path] = future.result()
+                print('[SUCCESS] %s'%(path))
+            except Exception as exc:
+                errors[path] = str(exc)
+                print('[ERROR]%s : %s'%(path, exc))
+    #return results
 
 
 if __name__ == '__main__':
-    # a = uploadImage('1.jpg')
-    # pprint(a)
-    b = uploadImages([r'1.jpg', r'2.png'])
-    pprint(b)
+    #a = uploadImage('1.jpg')
+    #pprint(a)
+    uploadImages([r'1.jpg', r'2.png', r'3.png', r'4.jpg', r'5.jpg', r'6.jpg'])
+    pprint(results)
+    pprint(errors)
+
+    
